1e6c5e5060863d8a7c952734058314d52c937d7b
Ram Saini <sainiram226@gmail.com>
2024-10-18 14:42:44 +0530

    scrollbar style

--- js/script.js
+++ js/script.js
@@ -360,3 +360,74 @@ yaxis: {
 
 var candleChart = new ApexCharts(document.querySelector("#chartCandle"), candleOptions);
 candleChart.render();
+
+
+
+var optionsLine = {
+  series: [{
+  data: data.slice()
+}],
+  chart: {
+  id: 'realtime',
+  height: 350,
+  type: 'line',
+  animations: {
+    enabled: true,
+    easing: 'linear',
+    dynamicAnimation: {
+      speed: 1000
+    }
+  },
+  toolbar: {
+    show: false
+  },
+  zoom: {
+    enabled: false
+  }
+},
+dataLabels: {
+  enabled: false
+},
+stroke: {
+  curve: 'smooth'
+},
+title: {
+  text: 'Dynamic Updating Chart',
+  align: 'left'
+},
+markers: {
+  size: 0
+},
+xaxis: {
+  type: 'datetime',
+  range: XAXISRANGE,
+},
+yaxis: {
+  max: 100
+},
+legend: {
+  show: false
+},
+};
+
+var chartL = new ApexCharts(document.querySelector("#chartLine"), optionsLine);
+chartL.render();
+
+
+var intervalRuns = 0;
+var interval = window.setInterval(function () {
+intervalRuns++
+getNewSeries(lastDate, {
+  min: 10,
+  max: 90
+})
+
+chartL.updateSeries([{
+  data: data
+}])
+
+if (intervalRuns === 2 && window.isATest === true) {
+  clearInterval(interval)
+}
+}, 1000)
+
